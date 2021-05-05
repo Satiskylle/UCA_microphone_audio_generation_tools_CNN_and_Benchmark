2cfa62274662c3e4bf07c4dd69ff412679d3376d
Satiskylle <satiskylle@gmail.com>
2021-05-05 12:38:18 +0200

    updated main script

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import scipy.signal as signal
 # defines (constants)
 MINIMUM_MICROPHONES_IN_ARRAY = 8
 
-
 #--------------------------------------------------------------------------
 # constants
 
@@ -47,33 +46,36 @@ def create_generated_audio_folder():
 def degrees_to_radians(angle):
     return angle * (math.pi / 180)
 
-def check_parameters_validity(mic_num, matrix_radius, doa_angle):
+def check_parameters_validity(mic_num, matrix_radius, sec_mic, wave_angle):
     if mic_num < MINIMUM_MICROPHONES_IN_ARRAY:
         print(mic_num + " microphones exceeds assert (max " + MINIMUM_MICROPHONES_IN_ARRAY + ")")
         return False
     
+    if sec_mic < 1 or sec_mic > mic_num:
+        print("Second microphone parameter is incorrect (second microphone: " + sec_mic + ")")
+        return False
+
     if matrix_radius < 0:
         print("Matrix radius is lower than 0 (matrix radius: " + matrix_radius + ")")
         return False
 
-    if doa_angle < 0 or doa_angle >= 360:
+    if wave_angle < 0 or wave_angle >= 360:
         print("Set DOA angle in range <0:360)")
         return False
 
 '''
-    @brief Calculates length difference between two specific microphones for specific angle of flat-wave
+    @brief Calculates length difference between first microphone and selected one for specific angle of flat-wave
     @note  difference is computed for angles <0:360) basing on ideal circled-microphone-matrix.
     @param [in] mic_num - number of microphones in matrix
     @param [in] matrix_radius - radius of matrix (length between center of matrix and microphone)
-    @param [in] first mic - first microphone. Must be less than @param mic_num
     @param [in] sec_mic - second microphone. Must be less than @param mic_num
     @param [in] wave_angle - angle specified in degrees in range <0:360)
 '''
-def calculate_audio_length_arrival_shift_between_two_microphones_for_specific_doa(mic_num, matrix_radius, first_mic, sec_mic, wave_angle):
-    if (check_parameters_validity(mic_num, matrix_radius, doa_angle) == False):
+def calculate_audio_length_arrival_shift_between_two_microphones_for_specific_doa(mic_num, matrix_radius, sec_mic, wave_angle):
+    if (check_parameters_validity(mic_num, matrix_radius, sec_mic, wave_angle) == False):
         return -1
 
-    alpha = 360 / mic_num
+    alpha = (sec_mic - 1) * (360 / mic_num)
     gamma = (180 - alpha) / 2
     print("alpha: " + str(alpha) + " gamma: " + str(gamma))
     cos_alpha = math.cos(degrees_to_radians(alpha))
@@ -102,8 +104,7 @@ def main():
     #print("Current directory: " + os.getcwd())
     #resample_specific_audio("../Database/tensorflow_recognition_challenge/train/audio/bed/00f0204f_nohash_0.wav", generated_audio_path + "/test.wav", 44100)
     #resample_specific_audio(generated_audio_path + "/test.wav", generated_audio_path + "/test_resampled_again.wav", 16000)
-    calculate_audio_length_arrival_shift_between_two_microphones_for_specific_doa(8, 1, 90 + (90 - 67.5))
-
+    calculate_audio_length_arrival_shift_between_two_microphones_for_specific_doa(8, 1, 3, 270)
 
     
 if __name__ == '__main__':
